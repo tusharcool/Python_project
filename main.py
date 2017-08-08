b05df29f2af869d44b838f04d797d8a3b5c95db1
tusharcool <tushargoel519@gmail.com>
2017-08-08 23:22:22 +0530

    VALIDATION COMPLETE

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 print 'Let\'s get started'
 spy_name = raw_input("what is ur name ?")
 if  len(spy_name)>0:
-    print "Alright" + spy_name +" I would like to know better before to proceed further.."
-    spy_salutation = raw_input('what is ur salutaion')
+    if spy_name.isalpha():
+        print "Alright" + spy_name +" I would like to know better before to proceed further.."
+        spy_salutation = raw_input('what is ur salutaion')
+        spy_name = spy_salutation + " " + spy_name
+        print 'welcome ' + spy_name + ' glad to here with you'
+        spy_age = 0
+        spy_rating = 0.0
+        spy_online = False
+        print type(spy_age)
+        spy_age = int(raw_input('Enter the age of spy '))
+        if(type(spy_age)== int):
+           print type(spy_age)
+           print 'valid age'
+        if spy_age > 15 and spy_age < 50:
+            spy_rating = raw_input("enter the rating")
+        else:
+            "not valid age"
+    else:
+        print"INVALID NAME"
 # concatination of salutation and name.
-    spy_name = spy_salutation + " " + spy_name
-    print 'welcome ' + spy_name + ' glad to here with you'
+
+
 else :
-    print 'Not valid input'
+    print 'Not valid in'
