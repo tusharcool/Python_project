8da8d0c77d1d0a86654e0cc79c7febea5947d069
tusharcool <tushargoel519@gmail.com>
2017-08-11 18:59:23 +0530

    function created

--- assignment.py
+++ assignment.py
@@ -1,13 +1,12 @@
-for i in range(2000,3200):
-    if(i%7==0 and i%5!=0):
-        print str(i) + "'"
+current=[i for i in range(2000,3200)  if(i%7==0 and i%5!=0)]
+print current
 
 str='Hello world'
 print str + 'test'
 
-list=[3,'dssd']
-print list*2
+list3=[3,'dssd']
+print list3*2
 
 list1=[5,6]
-list2=list+list1
+list2=list3+list1
 print list2
